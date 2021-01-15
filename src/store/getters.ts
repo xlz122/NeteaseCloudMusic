b728b4ae815a88453b5d7833b97bc71496b685bd
zlylm <912924984@qq.com>
2021-01-15 22:41:04 +0800

    fix:修复扫码登录bug

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -8,27 +8,27 @@ const getters: Getters<State> = {
   heaerActiveIndex: state => state.heaerActiveIndex,
   loginDialog: state => state.loginDialog,
   accountInfo: state => {
-	let {accountInfo} = state
-	if (Object.prototype.toString.call(accountInfo) === '[object Object]') {
-		return accountInfo
-	}
-	let aInfo = JSON.parse(state.accountInfo as string)
-	return Object.keys(aInfo).length > 0 ? JSON.parse(aInfo) : {}
+    const { accountInfo } = state;
+    if (Object.prototype.toString.call(accountInfo) === '[object Object]') {
+      return accountInfo;
+    }
+    const aInfo = JSON.parse(state.accountInfo as string);
+    return Object.keys(aInfo).length > 0 ? JSON.parse(aInfo) : {};
   },
   userInfo: state => {
-	let {userInfo} = state
-	if (Object.prototype.toString.call(userInfo) === '[object Object]') {
-		return userInfo
-	}
-	let uInfo = JSON.parse(state.userInfo as string)
-	return Object.keys(uInfo).length > 0 ? uInfo : {}
+    const { userInfo } = state;
+    if (Object.prototype.toString.call(userInfo) === '[object Object]') {
+      return userInfo;
+    }
+    const uInfo = JSON.parse(state.userInfo as string);
+    return Object.keys(uInfo).length > 0 ? uInfo : {};
   },
   isLogin: state => {
-	let {userInfo} = state
-	if (Object.prototype.toString.call(userInfo) !== '[object Object]') {
-		userInfo = JSON.parse(userInfo as string)
-	}
-	return Object.keys(userInfo as object).length > 0 ? true : false // 是否登录
+    let { userInfo } = state;
+    if (Object.prototype.toString.call(userInfo) !== '[object Object]') {
+      userInfo = JSON.parse(userInfo as string);
+    }
+    return Object.keys(userInfo as object).length > 0 ? true : false; // 是否登录
   }
 };
 
