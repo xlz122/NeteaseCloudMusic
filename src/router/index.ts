d2062b797f4f7024346135ab12584d65af151b14
xlz122 <122874722@qq.com>
2021-01-16 18:46:44 +0800

    feat:我的音乐-未登录时静态页面完成

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,11 @@ const routes: Array<RouteRecordRaw> = [
     component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
     children: [
       {
+        path: '/my-music',
+        name: 'my-music',
+        component: () => import('../views/my-music/MyMusic.vue')
+      },
+      {
         path: '/download',
         name: 'download',
         component: () => import('../views/download/Download.vue')
