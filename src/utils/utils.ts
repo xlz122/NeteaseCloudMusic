b0283d2357bb7d82481a24cbc4fd275f4e17b83b
xlz122 <122874722@qq.com>
2021-01-18 17:57:18 +0800

    feat:我的音乐-创建歌单详情部分完成

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -29,3 +29,50 @@ export function timeStampToDuration(timeStamp: number): string {
   }
   return ok;
 }
+
+/**
+ * @desc 日期格式化
+ * @param { String | Date } - date 日期或者时间戳
+ * @param { String } - fmt 格式化后的日期字符串格式
+ */
+export function formatDateTime(
+  date: string | Date,
+  fmt = 'yyyy-MM-dd hh:mm:ss'
+) {
+  if (!date) {
+    return '';
+  }
+  if (typeof date === 'number') {
+    date = new Date(date * 1000);
+  }
+  const o = {
+    'M+': (date as Date).getMonth() + 1, // 月份
+    'd+': (date as Date).getDate(), // 日
+    'h+': (date as Date).getHours(), // 小时
+    'm+': (date as Date).getMinutes(), // 分
+    's+': (date as Date).getSeconds(), // 秒
+    'q+': Math.floor(((date as Date).getMonth() + 3) / 3), // 季度
+    // eslint-disable-next-line
+    'S': (date as Date).getMilliseconds() // 毫秒
+  };
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt
+      .replace(RegExp.$1, (date as Date).getFullYear() + '')
+      .substr(4 - RegExp.$1.length);
+  }
+  for (const k in o) {
+    /* eslint-disable */
+    if (new RegExp('(' + k + ')').test(fmt)) {
+      fmt = fmt
+        .replace(
+          RegExp.$1, (RegExp.$1.length == 1)
+            // @ts-ignore
+            ? o[k]
+            // @ts-ignore
+            : ('00' + o[k]).substr(('' + o[k]).length)
+        );
+    }
+    /* eslint-enable */
+  }
+  return fmt;
+}
---
+++ src/views/my-music/PlayListMain.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="play-list-main">
+    <div class="main-header">
+      <div class="header-info">
+        <div class="info-img">
+          <img class="img" :src="playDetailData?.playlist?.coverImgUrl" />
+          <i class="icon"></i>
+        </div>
+        <div class="info-right">
+          <div class="title">
+            <i class="icon-title"></i>
+            <h2 class="title-text">{{ playDetailData?.playlist?.name }}</h2>
+          </div>
+          <div class="user-info">
+            <img
+              class="user-avatar"
+              :src="playDetailData?.playlist?.creator?.avatarUrl"
+            />
+            <span class="user-name">{{
+              playDetailData?.playlist?.creator?.nickname
+            }}</span>
+            <span class="user-time">
+              {{
+                formatDateTime(
+                  playDetailData?.playlist?.createTime / 1000,
+                  'yyyy-MM-dd'
+                )
+              }}
+              创建
+            </span>
+          </div>
+          <div class="operate-btn">
+            <div class="play">
+              <icon class="icon-play">播放</icon>
+            </div>
+            <div class="play-add"></div>
+            <div class="other">
+              <span class="icon">收藏</span>
+            </div>
+            <div class="other share">
+              <span class="icon">分享</span>
+            </div>
+            <div class="other download">
+              <span class="icon">下载</span>
+            </div>
+            <div class="other comment">
+              <span class="icon">评论</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="list-title">
+      <h3 class="title-text">歌曲列表</h3>
+      <span class="title-text-num">
+        {{ playDetailData?.playlist?.trackCount }}首歌
+      </span>
+      <div class="title-play-num">
+        播放:
+        <span class="eye-catching">{{
+          playDetailData?.playlist?.playCount
+        }}</span>
+        次
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { formatDateTime } from '@utils/utils.ts';
+
+export default defineComponent({
+  props: ({
+    playDetailData: {
+      type: Object,
+      default: {}
+    }
+  } as unknown) as undefined,
+  setup() {
+    return {
+      formatDateTime
+    };
+  }
+});
+</script>
+
+<style lang="less" scoped>
+@import './play-list-main.less';
+</style>
