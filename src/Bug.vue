dd24b00906b7a509eecc7548f565421ed3877d7b
xlz122 <122874722@qq.com>
2021-02-04 17:53:21 +0800

    fix: 音乐人页面暂时改为bug 未完成列表

---
+++ src/Bug.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="bug">
+    <p class="title">bug列表</p>
+    <ul>
+      <li class="item" v-for="(item, index) in bugList" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+    <p class="title">未完成列表</p>
+    <ul>
+      <li class="item" v-for="(item, index) in incompleteFunction" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    // bug
+    const bugList = ref<string[]>([
+      '歌单详情 - 个人信息部分，按钮是否禁用样式问题',
+      '删除收藏的歌单，获取详情报错'
+    ]);
+    // 未完成
+    const incompleteFunction = ref<string[]>(['邮箱登录', '手机号登录']);
+    return {
+      bugList,
+      incompleteFunction
+    };
+  }
+});
+</script>
+
+<style lang="less" scoped>
+.bug {
+  width: 980px;
+  margin: 0 auto;
+  text-align: left;
+  .title {
+    margin-top: 20px;
+    font-size: 16px;
+  }
+  .item {
+    margin: 8px 0;
+  }
+}
+</style>
