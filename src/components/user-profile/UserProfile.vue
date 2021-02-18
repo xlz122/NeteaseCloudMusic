075cf61d672abb009ecfdc1894ec3967ec40afe6
xlz122 <122874722@qq.com>
2021-02-18 15:45:10 +0800

    feat: 完成首页签到功能

--- src/components/user-profile/UserProfile.vue
+++ src/components/user-profile/UserProfile.vue
@@ -4,36 +4,37 @@
     <div class="user-profile-container">
       <div class="user-info">
         <div class="user-avatar">
-          <img
-            class="user-avatar-img"
-            src="http://p1.music.126.net/uAJkvQYbojqFXHZrjKz97w==/109951164336447751.jpg?param=80y80"
-          />
+          <img class="user-avatar-img" :src="userInfo?.profile?.avatarUrl" />
         </div>
         <div class="info">
           <h4 class="name">
-            <span class="text">撇捺z</span>
+            <span class="text">{{ userInfo?.profile?.nickname }}</span>
             <i class="icon-info"></i>
           </h4>
           <p class="lv">
-            <i class="lv-icon-left">7</i>
+            <i class="lv-icon-left">{{ userInfo?.level }}</i>
             <i class="lv-icon-right"></i>
           </p>
-          <button class="btnwarp">
+          <button
+            class="btnwarp"
+            :class="{ 'disable-btnwarp': isSignIn }"
+            @click="signIn"
+          >
             <i class="btnwarp-icon">签到</i>
           </button>
         </div>
       </div>
       <ul class="list">
         <li class="item first-item">
-          <strong class="num">0</strong>
+          <strong class="num">{{ userInfo?.profile?.eventCount }}</strong>
           <span class="text">动态</span>
         </li>
         <li class="item">
-          <strong class="num">0</strong>
+          <strong class="num">{{ userInfo?.profile?.follows }}</strong>
           <span class="text">关注</span>
         </li>
         <li class="item last-item">
-          <strong class="num">0</strong>
+          <strong class="num">{{ userInfo?.profile?.followeds }}</strong>
           <span class="text">粉丝</span>
         </li>
       </ul>
@@ -45,9 +46,7 @@
       <p class="login-title">
         登录网易云音乐，可以享受无限收藏的乐趣，并且无限同步到手机
       </p>
-      <button class="login-btn" @click="openLogin">
-        用户登录
-      </button>
+      <button class="login-btn" @click="openLogin">用户登录</button>
     </div>
   </div>
 </template>
@@ -55,6 +54,8 @@
 <script lang="ts">
 import { defineComponent, computed } from 'vue';
 import { useStore } from 'vuex';
+import { dailySignin } from '@api/home';
+import { ResponseType } from '@/types/types';
 
 export default defineComponent({
   setup() {
@@ -63,15 +64,37 @@ export default defineComponent({
     // 是否登录
     const isLogin = computed(() => $store.getters.isLogin);
 
+    // 用户信息
+    const userInfo = computed(() => $store.getters.userInfo);
+
+    // 是否已签到
+    const isSignIn = computed(() => $store.getters.userInfo.pcSign);
+
+    // 签到
+    function signIn(): boolean | undefined {
+      // 防止重复签到
+      if (isSignIn.value) {
+        return false;
+      }
+      dailySignin().then((res: ResponseType) => {
+        if (res.code === 200) {
+          $store.commit('setSignIn', true);
+        }
+      })
+    }
+
     // 打开登录对话框
     function openLogin(): void {
       $store.commit('setLoginDialog', true);
     }
     return {
       isLogin,
-      openLogin
+      userInfo,
+      isSignIn,
+      signIn,
+      openLogin,
     };
-  }
+  },
 });
 </script>
 
