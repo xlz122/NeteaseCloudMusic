0fe52fb2913b29d7d9582445e014f8daad467f10
xlz122 <122874722@qq.com>
2021-02-18 11:37:45 +0800

    feat: 创作者中心登录后页面完成

---
+++ src/views/header/creator-center/CreatorCenter.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="creator-center">
+    <div class="creator-container">
+      <p class="title">创作者中心</p>
+      <p class="desc">· 请选择身份进入管理平台 ·</p>
+      <ul class="list">
+        <li class="item musician">
+          <a
+            class="link"
+            href="https://music.163.com/st/musician"
+            target="_blank"
+          >
+            <i class="icon"></i>
+            <p class="typename">网易音乐人</p>
+            <p class="intro">欢迎所有音乐“妄想”</p>
+          </a>
+        </li>
+        <li class="item talent">
+          <a
+            class="link"
+            href="https://music.163.com/st/creator"
+            target="_blank"
+          >
+            <i class="icon"></i>
+            <p class="typename">云音乐达人</p>
+            <p class="intro">用内容创作玩转音乐</p>
+          </a>
+        </li>
+        <li class="item anchor">
+          <a class="link" href="https://look.163.com/" target="_blank">
+            <i class="icon"></i>
+            <p class="typename">LOOK主播</p>
+            <p class="intro">让世界看见特别的你</p>
+          </a>
+        </li>
+      </ul>
+      <a
+        class="creator-company"
+        href="https://music.163.com/musician/company/entrance"
+        target="_blank"
+      >
+        <div class="creator-company-title">
+          <span class="text">公司</span>
+          <span class="line">/</span>
+          <span class="text">厂牌</span>
+        </div>
+        <div class="intro">赋能厂牌数字化管理，发挥音乐无限可能</div>
+        <div class="company-enter">进入</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+@import './creator-center.less';
+</style>
