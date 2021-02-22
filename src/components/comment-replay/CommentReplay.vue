3634ffa48298ec1bb4ebf2971097c394e1abf3b7
xlz122 <122874722@qq.com>
2021-02-22 17:07:56 +0800

    feat: 封装评论组件

---
+++ src/components/comment-replay/CommentReplay.vue
@@ -0,0 +1,230 @@
+<template>
+  <div class="comment-replay">
+    <div class="comment-content">
+      <!-- :placeholder="`回复${nickname}:`" -->
+      <textarea
+        class="comment-textarea"
+        v-model="replay.text"
+        :rows="rows"
+        :style="commentReplayStyle"
+      ></textarea>
+    </div>
+    <div class="operate">
+      <div class="operate-icon">
+        <i class="icon expression" @click="isEmoj"></i>
+        <i class="icon att"></i>
+      </div>
+      <div class="operate-publish">
+        <span class="text" :class="{ 'words-beyond': replay.length < 0 }">
+          {{ replay.length }}
+        </span>
+        <button class="publish" @click="submit">评论</button>
+      </div>
+    </div>
+    <!--表情-->
+    <div v-if="expressionShow" class="auto-content">
+      <div class="e-emts">
+        <div class="emtwrap">
+          <span
+            class="emtwrap-img"
+            v-for="(item, index) in exproessionPage.list"
+            :key="index"
+            :title="item.title"
+          >
+            <img
+              class="emtwrap-expression"
+              :src="
+                `http://s1.music.126.net/style/web2/emt/emoji_${item.num}.png`
+              "
+              @click="chooseEmoj(index)"
+            />
+          </span>
+        </div>
+        <div class="page">
+          <a
+            @click="expressionSwitch('prev')"
+            href="#"
+            hidefocus="true"
+            class="j-flag u-btn u-btn-prv"
+          ></a>
+          <span class="j-flag text s-fc3">
+            {{ exproessionPage.current }} / {{ exproessionPage.total }}
+          </span>
+          <a
+            @click="expressionSwitch('next')"
+            href="#"
+            hidefocus="true"
+            class="j-flag u-btn u-btn-nxt"
+          ></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, reactive, watch, computed } from 'vue';
+import { onMounted, onUnmounted } from 'vue';
+import { expressionList } from './comment-replay';
+
+interface Replay {
+  text: string;
+  length: number;
+}
+
+interface ExproessionPage {
+  list: unknown;
+  current: number;
+  total: number;
+}
+
+export default defineComponent({
+  props: ({
+    clearText: {
+      type: Boolean,
+      default: false
+    },
+    rows: {
+      type: [Number, String],
+      default: 1
+    },
+    width: {
+      type: [Number, String],
+      default: 300
+    },
+    nickname: {
+      type: String,
+      default: ''
+    }
+  } as unknown) as undefined,
+  setup(
+    props: {
+      clearText: boolean;
+      width: number | string;
+      height: number | string;
+    },
+    { emit }
+  ) {
+    // 样式设置
+    const commentReplayStyle = computed(() => {
+      return {
+        width: `${props.width}px`
+      };
+    });
+    // 回复数据
+    const replay = reactive<Replay>({
+      text: '',
+      length: 140
+    });
+
+    // 监听最大可以输入数量
+    watch(
+      () => replay.text,
+      (curVal: string) => {
+        replay.length = 140 - curVal.length;
+      }
+    );
+
+    // 监听清除文本内容
+    watch(
+      () => props.clearText,
+      (curVal: boolean) => {
+        if (curVal) {
+          replay.text = '';
+        }
+      }
+    );
+
+    // 表情显隐
+    const expressionShow = ref<boolean>(false);
+    // 分页数据
+    const exproessionPage = reactive<ExproessionPage>({
+      list: [],
+      current: 1,
+      total: 0
+    });
+    function isEmoj(): void {
+      expressionShow.value = !expressionShow.value;
+      // 显示的时候计算页数
+      if (expressionShow.value) {
+        exproessionPage.list = expressionList.slice(0, 50);
+        exproessionPage.current = 1;
+        exproessionPage.total = Math.ceil(expressionList.length / 50);
+      }
+    }
+
+    // 表情页数切换
+    function expressionSwitch(type: string): boolean | undefined {
+      // 上一页
+      if (type === 'prev') {
+        if (exproessionPage.current === 0) {
+          return false;
+        }
+        exproessionPage.current--;
+        exproessionPage.list = expressionList.slice(0, 50);
+      }
+      // 下一页
+      if (type === 'next') {
+        if (exproessionPage.current === exproessionPage.total) {
+          return false;
+        }
+        exproessionPage.current++;
+        exproessionPage.list = expressionList.slice(
+          (exproessionPage.current - 1) * 50,
+          (exproessionPage.current - 1) * 50 + 50
+        );
+      }
+    }
+
+    // 表情点击
+    function chooseEmoj(index: number): void {
+      const list = JSON.parse(JSON.stringify(exproessionPage.list));
+      replay.text += `[${list[index].title}]`;
+    }
+
+    // 回复提交
+    function submit(): void {
+      emit('submit', replay.text);
+    }
+
+    // 监听点击
+    onMounted(() => {
+      document.addEventListener('click', function(e: MouseEvent): void {
+        const target = e.target as HTMLElement;
+        // 点击表情以外得地方，关闭表情弹框
+        if (
+          target.className !== 'icon expression' &&
+          target.className !== 'page' &&
+          target.className !== 'j-flag u-btn u-btn-prv' &&
+          target.className !== 'j-flag u-btn u-btn-nxt' &&
+          target.className !== 'emtwrap-expression'
+        ) {
+          expressionShow.value = false;
+        }
+      });
+    });
+
+    onUnmounted(() => {
+      document.removeEventListener('click', function(): void {
+        console.log('download.vue 点击事件移除');
+      });
+    });
+
+    return {
+      expressionList,
+      commentReplayStyle,
+      replay,
+      expressionShow,
+      isEmoj,
+      exproessionPage,
+      expressionSwitch,
+      chooseEmoj,
+      submit
+    };
+  }
+});
+</script>
+
+<style lang="less" scoped>
+@import './comment-replay.less';
+</style>
---
+++ src/components/comment-replay/comment-replay.ts
@@ -0,0 +1,247 @@
+export interface ExpressionList {
+  title: string;
+  num: number;
+}
+
+export const expressionList: ExpressionList[] = [
+  {
+    title: '大笑',
+    num: 86
+  },
+  {
+    title: '可爱',
+    num: 85
+  },
+  {
+    title: '憨笑',
+    num: 359
+  },
+  {
+    title: '色',
+    num: 95
+  },
+  {
+    title: '亲亲',
+    num: 363
+  },
+  {
+    title: '惊恐',
+    num: 96
+  },
+  {
+    title: '流泪',
+    num: 356
+  },
+  {
+    title: '亲',
+    num: 362
+  },
+  {
+    title: '呆',
+    num: 352
+  },
+  {
+    title: '哀伤',
+    num: 342
+  },
+  {
+    title: '呲牙',
+    num: 343
+  },
+  {
+    title: '吐舌',
+    num: 348
+  },
+  {
+    title: '撇嘴',
+    num: 361
+  },
+  {
+    title: '怒',
+    num: 353
+  },
+  {
+    title: '奸笑',
+    num: 341
+  },
+  {
+    title: '汗',
+    num: 97
+  },
+  {
+    title: '痛苦',
+    num: 346
+  },
+  {
+    title: '惶恐',
+    num: 354
+  },
+  {
+    title: '生病',
+    num: 350
+  },
+  {
+    title: '口罩',
+    num: 351
+  },
+  {
+    title: '大哭',
+    num: 357
+  },
+  {
+    title: '晕',
+    num: 355
+  },
+  {
+    title: '发怒',
+    num: 115
+  },
+  {
+    title: '开心',
+    num: 360
+  },
+  {
+    title: '鬼脸',
+    num: 94
+  },
+  {
+    title: '皱眉',
+    num: 87
+  },
+  {
+    title: '流感',
+    num: 358
+  },
+  {
+    title: '爱心',
+    num: 33
+  },
+  {
+    title: '心碎',
+    num: 34
+  },
+  {
+    title: '钟情',
+    num: 303
+  },
+  {
+    title: '星星',
+    num: 309
+  },
+  {
+    title: '生气',
+    num: 314
+  },
+  {
+    title: '便便',
+    num: 89
+  },
+  {
+    title: '强',
+    num: 13
+  },
+  {
+    title: '弱',
+    num: 372
+  },
+  {
+    title: '拜',
+    num: 14
+  },
+  {
+    title: '牵手',
+    num: 379
+  },
+  {
+    title: '跳舞',
+    num: 380
+  },
+  {
+    title: '禁止',
+    num: 374
+  },
+  {
+    title: '这边',
+    num: 262
+  },
+  {
+    title: '爱意',
+    num: 106
+  },
+  {
+    title: '示爱',
+    num: 376
+  },
+  {
+    title: '嘴唇',
+    num: 367
+  },
+  {
+    title: '狗',
+    num: 81
+  },
+  {
+    title: '猫',
+    num: 78
+  },
+  {
+    title: '猪',
+    num: 100
+  },
+  {
+    title: '兔子',
+    num: 459
+  },
+  {
+    title: '小鸡',
+    num: 450
+  },
+  {
+    title: '公鸡',
+    num: 461
+  },
+  {
+    title: '幽灵',
+    num: 116
+  },
+  {
+    title: '圣诞',
+    num: 411
+  },
+  {
+    title: '外星',
+    num: 101
+  },
+  {
+    title: '钻石',
+    num: 52
+  },
+  {
+    title: '礼物',
+    num: 107
+  },
+  {
+    title: '男孩',
+    num: 0
+  },
+  {
+    title: '女孩',
+    num: 1
+  },
+  {
+    title: '蛋糕',
+    num: 337
+  },
+  {
+    title: '18',
+    num: 186
+  },
+  {
+    title: '圈',
+    num: 312
+  },
+  {
+    title: '叉',
+    num: 313
+  }
+];
