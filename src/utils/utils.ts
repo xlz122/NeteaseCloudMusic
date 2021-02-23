2beee28acadaa786cc6a2d25e697e0a54f717e8e
xlz122 <122874722@qq.com>
2021-02-23 15:33:12 +0800

    fix: 修复不是当前用户的评论也出现删除按钮的bug

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -209,11 +209,10 @@ export function formatDate(timestamp: number): string {
  * 大数字转换，将大额数字转换为万、千万、亿等
  * @param value 数字值
  */
-export function bigNumberTransform(value: number): boolean | string {
-  let newValue = Number(value);
-  let result = '';
+export function bigNumberTransform(value: number): number | string {
+  const newValue = Number(value);
   if (isNaN(newValue)) {
-    return false;
+    return value;
   }
   // 小于1万
   if (newValue < 10000) {
@@ -221,15 +220,15 @@ export function bigNumberTransform(value: number): boolean | string {
   }
   // 小于1亿
   if (newValue < 100000000) {
-    return result = (newValue / 10000).toFixed(1) + '万';
+    return (newValue / 10000).toFixed(1) + '万';
   }
   // 小于10亿
   if (newValue < 1000000000) {
-    return result = (newValue / 10000).toFixed(0) + '万';
+    return (newValue / 10000).toFixed(0) + '万';
   }
   // 大于10亿
   if (newValue > 1000000000) {
-    return result = (newValue / 10000).toFixed(0) + '万';
+    return (newValue / 10000).toFixed(0) + '万';
   }
-  return result;
+  return newValue;
 }
