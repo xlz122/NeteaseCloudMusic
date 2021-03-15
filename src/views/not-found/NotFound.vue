358571c21c15b24d1edd363891feac23f1b0d75e
xlz122 <122874722@qq.com>
2021-03-15 13:56:46 +0800

    feat: 404页面优化

--- src/views/not-found/NotFound.vue
+++ src/views/not-found/NotFound.vue
@@ -1,3 +1,25 @@
 <template>
-  <div>404页面</div>
+  <div class="not-found">
+    未找到该页面，将在3秒后返回首页...
+  </div>
 </template>
+
+<script lang="ts" setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+setTimeout(() => {
+  router.push({ name: 'home' });
+}, 3000);
+</script>
+
+<style lang="less" scoped>
+.not-found {
+  position: fixed;
+  top: 0%;
+  left: 0%;
+  color: #000;
+  font-size: 16px;
+}
+</style>
