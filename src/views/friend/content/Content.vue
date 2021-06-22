618e7b841a9d3d0ea5887a4071691e544f73c24a
xlz122 <122874722@qq.com>
2021-06-22 11:22:03 +0800

    feat: 朋友（登录后）专辑 歌单 文案匹配

--- src/views/friend/content/Content.vue
+++ src/views/friend/content/Content.vue
@@ -82,6 +82,156 @@
             </div>
           </div>
         </template>
+        <!-- 专辑 -->
+        <template v-if="item.type === 19">
+          <div class="cover">
+            <img
+              class="img"
+              :src="`${item?.user?.avatarUrl}?param=45y45`"
+              alt=""
+            />
+          </div>
+          <div class="info">
+            <div class="info-title">
+              <span class="t-text">{{ item?.user?.nickname }}</span>
+              <i class="t-icon"></i>
+              <span class="t-desc">分享专辑</span>
+            </div>
+            <div class="time">{{ formatDate(item?.showTime) }}</div>
+            <div class="mood" v-html="item?.json?.msg"></div>
+            <div class="music-info">
+              <div class="music-cover">
+                <img
+                  class="cover-img"
+                  :src="`${item?.json?.album?.picUrl}?param=40y40`"
+                  alt=""
+                />
+                <i class="play-icon"></i>
+              </div>
+              <div class="music-detail">
+                <h3 class="music-detail-title">{{ item?.json?.song?.name }}</h3>
+                <h4 class="music-detail-name">
+                  {{ item?.json?.song?.artists[0].name }}
+                </h4>
+              </div>
+            </div>
+            <div class="operate">
+              <span class="operate-btn">
+                <i
+                  class="operate-btn-icon operate-btn-active-icon"
+                  v-if="item?.info?.liked"
+                  @click="setDynamicLike(item.id, item.info.threadId, 0)"
+                ></i>
+                <i
+                  class="operate-btn-icon"
+                  v-else
+                  @click="setDynamicLike(item.id, item.info.threadId, 1)"
+                ></i>
+                <span
+                  v-if="item?.info?.likedCount > 0"
+                  class="operate-btn-text"
+                >
+                  ({{ item?.info?.likedCount }})
+                </span>
+              </span>
+              <span class="operate-line">|</span>
+              <span class="operate-btn">
+                <span class="operate-btn-text">转发</span>
+                <span
+                  class="operate-btn-text"
+                  v-if="item?.insiteForwardCount > 0"
+                >
+                  ({{ item?.insiteForwardCount }})
+                </span>
+              </span>
+              <span class="operate-line">|</span>
+              <span class="operate-btn">
+                <span class="operate-btn-text">评论</span>
+                <span
+                  class="operate-btn-text"
+                  v-if="item?.info?.commentThread?.commentCount > 0"
+                >
+                  ({{ item?.info?.commentThread?.commentCount }})
+                </span>
+              </span>
+            </div>
+          </div>
+        </template>
+        <!-- 歌单 -->
+        <template v-if="item.type === 13">
+          <div class="cover">
+            <img
+              class="img"
+              :src="`${item?.user?.avatarUrl}?param=45y45`"
+              alt=""
+            />
+          </div>
+          <div class="info">
+            <div class="info-title">
+              <span class="t-text">{{ item?.user?.nickname }}</span>
+              <i class="t-icon"></i>
+              <span class="t-desc">分享歌单</span>
+            </div>
+            <div class="time">{{ formatDate(item?.showTime) }}</div>
+            <div class="mood" v-html="item?.json?.msg"></div>
+            <div class="music-info">
+              <div class="music-cover">
+                <img
+                  class="cover-img"
+                  :src="`${item?.json?.playlist?.coverImgUrl}?param=40y40`"
+                  alt=""
+                />
+                <i class="play-icon"></i>
+              </div>
+              <div class="music-detail">
+                <h3 class="music-detail-title">{{ item?.json?.song?.name }}</h3>
+                <h4 class="music-detail-name">
+                  {{ item?.json?.song?.artists[0].name }}
+                </h4>
+              </div>
+            </div>
+            <div class="operate">
+              <span class="operate-btn">
+                <i
+                  class="operate-btn-icon operate-btn-active-icon"
+                  v-if="item?.info?.liked"
+                  @click="setDynamicLike(item.id, item.info.threadId, 0)"
+                ></i>
+                <i
+                  class="operate-btn-icon"
+                  v-else
+                  @click="setDynamicLike(item.id, item.info.threadId, 1)"
+                ></i>
+                <span
+                  v-if="item?.info?.likedCount > 0"
+                  class="operate-btn-text"
+                >
+                  ({{ item?.info?.likedCount }})
+                </span>
+              </span>
+              <span class="operate-line">|</span>
+              <span class="operate-btn">
+                <span class="operate-btn-text">转发</span>
+                <span
+                  class="operate-btn-text"
+                  v-if="item?.insiteForwardCount > 0"
+                >
+                  ({{ item?.insiteForwardCount }})
+                </span>
+              </span>
+              <span class="operate-line">|</span>
+              <span class="operate-btn">
+                <span class="operate-btn-text">评论</span>
+                <span
+                  class="operate-btn-text"
+                  v-if="item?.info?.commentThread?.commentCount > 0"
+                >
+                  ({{ item?.info?.commentThread?.commentCount }})
+                </span>
+              </span>
+            </div>
+          </div>
+        </template>
         <!-- 话题 -->
         <template v-if="item.type === 33">
           <div class="cover">
@@ -97,6 +247,65 @@
               </span>
             </div>
             <i class="topic-close"></i>
+          </div>
+        </template>
+        <!-- 文案 -->
+        <template v-if="item.type === 35">
+          <div class="cover">
+            <img
+              class="img"
+              :src="`${item?.user?.avatarUrl}?param=45y45`"
+              alt=""
+            />
+          </div>
+          <div class="info">
+            <div class="info-title">
+              <span class="t-text">{{ item?.user?.nickname }}</span>
+              <i class="t-icon"></i>
+              <span class="t-desc"></span>
+            </div>
+            <div class="time">{{ formatDate(item?.showTime) }}</div>
+            <div class="mood" v-html="item?.json?.msg"></div>
+            <div class="operate">
+              <span class="operate-btn">
+                <i
+                  class="operate-btn-icon operate-btn-active-icon"
+                  v-if="item?.info?.liked"
+                  @click="setDynamicLike(item.id, item.info.threadId, 0)"
+                ></i>
+                <i
+                  class="operate-btn-icon"
+                  v-else
+                  @click="setDynamicLike(item.id, item.info.threadId, 1)"
+                ></i>
+                <span
+                  v-if="item?.info?.likedCount > 0"
+                  class="operate-btn-text"
+                >
+                  ({{ item?.info?.likedCount }})
+                </span>
+              </span>
+              <span class="operate-line">|</span>
+              <span class="operate-btn">
+                <span class="operate-btn-text">转发</span>
+                <span
+                  class="operate-btn-text"
+                  v-if="item?.insiteForwardCount > 0"
+                >
+                  ({{ item?.insiteForwardCount }})
+                </span>
+              </span>
+              <span class="operate-line">|</span>
+              <span class="operate-btn">
+                <span class="operate-btn-text">评论</span>
+                <span
+                  class="operate-btn-text"
+                  v-if="item?.info?.commentThread?.commentCount > 0"
+                >
+                  ({{ item?.info?.commentThread?.commentCount }})
+                </span>
+              </span>
+            </div>
           </div>
         </template>
       </li>
@@ -127,7 +336,6 @@ export default defineComponent({
             }
           });
           eventList.value = res.event;
-          console.log(eventList.value);
         }
       });
     }
