44ee82951367efd9e1d55f9b106767e7c290efe7
xlz122 <122874722@qq.com>
2021-06-25 17:21:01 +0800

    fix: 404页面setup语法糖替换成普通写法,解决打包部署后,template不渲染bug

--- src/views/not-found/NotFound.vue
+++ src/views/not-found/NotFound.vue
@@ -4,21 +4,26 @@
   </div>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts">
+import { defineComponent } from 'vue';
 import { useRouter } from 'vue-router';
 
-const router = useRouter();
+export default defineComponent({
+  setup() {
+    const router = useRouter();
 
-setTimeout(() => {
-  router.push({ name: 'home' });
-}, 3000);
+    setTimeout(() => {
+      router.push({ name: 'home' });
+    }, 3000);
+  }
+});
 </script>
 
 <style lang="less" scoped>
 .not-found {
   position: fixed;
-  top: 0%;
-  left: 0%;
+  top: 0;
+  left: 0;
   color: #000;
   font-size: 16px;
 }
