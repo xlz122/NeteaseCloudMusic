b6f4ac72c7a499da7070c7d1e3bff74e6a87c575
xlz122 <122874722@qq.com>
2021-07-03 11:37:41 +0800

    feat: 歌手-菜单完成

--- src/views/home-singer/singer-menu/SingerMenu.vue
+++ src/views/home-singer/singer-menu/SingerMenu.vue
@@ -1,40 +1,193 @@
 <template>
   <div class="singer-menu">
-    <div class="group">
-      <h2 class="group-title">推荐</h2>
+    <div
+      class="group"
+      v-for="(item, index) in menu"
+      :key="index"
+      :class="{ 'language': index !== 0 }"
+    >
+      <h2 class="group-title">{{ item.title }}</h2>
       <ul class="group-list">
-        <li class="item">
-          <span class="item-text item-active-text">推荐歌手</span>
-        </li>
-        <li class="item">
-          <span class="item-text">入驻歌手</span>
-        </li>
-      </ul>
-    </div>
-    <div class="group language">
-      <h2 class="group-title">华语</h2>
-      <ul class="group-list">
-        <li class="item">
-          <span class="item-text">华语女歌手</span>
-        </li>
-        <li class="item">
-          <span class="item-text">华语男歌手</span>
-        </li>
-      </ul>
-    </div>
-    <div class="group language">
-      <h2 class="group-title">华语</h2>
-      <ul class="group-list">
-        <li class="item">
-          <span class="item-text">华语女歌手</span>
-        </li>
-        <li class="item">
-          <span class="item-text">华语男歌手</span>
+        <li
+          class="item"
+          v-for="(i, ind) in item?.list"
+          :key="ind"
+          @click="menuSelete(item, i.type)"
+        >
+          <span
+            class="item-text"
+            :class="{ 'item-active-text': i.selected }"
+          >
+            {{ i.title }}
+          </span>
         </li>
       </ul>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+type Menu = {
+  type: number,
+  title: string;
+  list: List[]
+};
+
+type List = {
+  type: number;
+  title: string;
+  selected: boolean;
+};
+
+export default defineComponent({
+  emits: ['menuSelete'],
+  setup(props, { emit }) {
+    const menu = ref<Menu[]>([
+      {
+        type: -8,
+        title: '推荐',
+        list: [
+          {
+            type: 1,
+            title: '推荐歌手',
+            selected: true
+          },
+          {
+            type: 2,
+            title: '入驻歌手',
+            selected: false
+          }
+        ]
+      },
+      {
+        type: 7,
+        title: '华语',
+        list: [
+          {
+            type: 1,
+            title: '华语男歌手',
+            selected: false
+          },
+          {
+            type: 2,
+            title: '华语女歌手',
+            selected: false
+          },
+          {
+            type: 3,
+            title: '华语组合/乐队',
+            selected: false
+          }
+        ]
+      },
+      {
+        type: 96,
+        title: '欧美',
+        list: [
+          {
+            type: 1,
+            title: '欧美男歌手',
+            selected: false
+          },
+          {
+            type: 2,
+            title: '欧美女歌手',
+            selected: false
+          },
+          {
+            type: 3,
+            title: '欧美组合/乐队',
+            selected: false
+          }
+        ]
+      },
+      {
+        type: 8,
+        title: '日本',
+        list: [
+          {
+            type: 1,
+            title: '日本男歌手',
+            selected: false
+          },
+          {
+            type: 2,
+            title: '日本女歌手',
+            selected: false
+          },
+          {
+            type: 3,
+            title: '日本组合/乐队',
+            selected: false
+          }
+        ]
+      },
+      {
+        type: 16,
+        title: '韩国',
+        list: [
+          {
+            type: 1,
+            title: '韩国男歌手',
+            selected: false
+          },
+          {
+            type: 2,
+            title: '韩国女歌手',
+            selected: false
+          },
+          {
+            type: 3,
+            title: '韩国组合/乐队',
+            selected: false
+          }
+        ]
+      },
+      {
+        type: 0,
+        title: '其他',
+        list: [
+          {
+            type: 1,
+            title: '其他男歌手',
+            selected: false
+          },
+          {
+            type: 2,
+            title: '其他女歌手',
+            selected: false
+          },
+          {
+            type: 3,
+            title: '其他组合/乐队',
+            selected: false
+          }
+        ]
+      }
+    ]);
+
+    function menuSelete(item: Menu, type: number): void {
+      menu.value.forEach((m: Menu) => {
+        m.list.forEach((l: List) => {
+          if (item.type === m.type && type === l.type) {
+            l.selected = true;
+          } else {
+            l.selected = false;
+          }
+        });
+      });
+      emit('menuSelete', item.type, type);
+    }
+
+    return {
+      menu,
+      menuSelete
+    };
+  }
+});
+</script>
 
 <style lang="less">
 .singer-menu {
