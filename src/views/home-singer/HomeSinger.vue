ce15b9f0b01d9d93dba7f20ab9f3d80eff3cf964
xlz122 <122874722@qq.com>
2021-07-03 11:37:41 +0800

    feat: 歌手-菜单完成

--- src/views/home-singer/HomeSinger.vue
+++ src/views/home-singer/HomeSinger.vue
@@ -2,7 +2,7 @@
   <div class="home-singer">
     <div class="home-singer-container">
       <div class="home-singer-menu">
-        <SingerMenu />
+        <SingerMenu @menuSelete="menuSelete" />
       </div>
       <div class="home-singer-content">
         <SingerContent />
@@ -13,6 +13,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useStore } from 'vuex';
 import SingerMenu from './singer-menu/SingerMenu.vue';
 import SingerContent from './singer-content/SingerContent.vue';
 
@@ -20,6 +21,21 @@ export default defineComponent({
   components: {
     SingerMenu,
     SingerContent
+  },
+  setup() {
+    const $store = useStore();
+
+    // 菜单选择
+    function menuSelete(area: number, type: number) {
+      $store.commit('setMessage', {
+        type: 'info',
+        title: `菜单选择：type:${type}, area:${area}`
+      });
+    }
+
+    return {
+      menuSelete
+    };
   }
 });
 </script>
