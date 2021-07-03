ce81b38dcbc70395b276638177c885092c7a5f6b
xlz122 <122874722@qq.com>
2021-07-03 11:55:37 +0800

    fix: 新增歌手接口文件

--- src/views/home-singer/HomeSinger.vue
+++ src/views/home-singer/HomeSinger.vue
@@ -12,8 +12,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive } from 'vue';
 import { useStore } from 'vuex';
+import { topArtists } from '@api/home-singer';
+import { ResponseType } from '@/type/type';
 import SingerMenu from './singer-menu/SingerMenu.vue';
 import SingerContent from './singer-content/SingerContent.vue';
 
@@ -25,12 +27,49 @@ export default defineComponent({
   setup() {
     const $store = useStore();
 
+    // 歌手数据
+    const singerList = reactive({
+      main: [],
+      second: []
+    });
+
     // 菜单选择
     function menuSelete(area: number, type: number) {
+      // 推荐歌手
+      if (area === -8 && type === 1) {
+        getTopArtists();
+        return false;
+      }
+      // 入驻歌手
+      if (area === -8 && type === 2) {
+        $store.commit('setMessage', {
+          type: 'error',
+          title: '入驻歌手暂无接口'
+        });
+        return false;
+      }
       $store.commit('setMessage', {
         type: 'info',
         title: `菜单选择：type:${type}, area:${area}`
       });
+    }
+
+    // 获取热门歌手数据
+    function getTopArtists(): void {
+      topArtists()
+        .then((res: ResponseType) => {
+          if (res.code === 200) {
+            
+          } else {
+            $store.commit('setMessage', {
+              type: 'error',
+              title: res?.msg
+            });
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
 
     return {
