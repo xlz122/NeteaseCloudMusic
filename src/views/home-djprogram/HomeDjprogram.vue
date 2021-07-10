0ebbcb45bf88cfceae052270aea8efc38b3d6229
xlz122 <122874722@qq.com>
2021-07-10 18:29:04 +0800

    feat: 增加电台页面文件

---
+++ src/views/home-djprogram/HomeDjprogram.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home-djprogram">
+    <div class="home-djprogram-container">
+      <div class="category-box">
+        <ul class="category-list"></ul>
+        <span class="btn prev-btn"></span>
+        <span class="btn next-btn"></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+// import { useStore } from 'vuex';
+// import { topPlaylist } from '@api/home-song-sheet';
+// import { ResponseType, LoopType } from '@/types/types';
+
+export default defineComponent({
+  setup() {
+    // const $route = useRoute();
+
+    return {};
+  }
+});
+</script>
+
+<style lang="less" scoped>
+@import './home-djprogram.less';
+</style>
