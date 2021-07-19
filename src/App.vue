ef112fade78fed93a80a71358a52988d1d7eb13b
xlz122 <122874722@qq.com>
2021-07-19 10:24:36 +0800

    fix: 修复嵌套路由，路由跳转组件渲染两次bug

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <router-view v-slot="{ Component }">
+  <router-view v-slot="{ Component }" v-if="$route.meta.keepAlive">
     <keep-alive>
-      <component :is="Component" v-if="$route.meta.keepAlive" />
+      <component :is="Component" />
     </keep-alive>
   </router-view>
-  <router-view v-if="!$route.meta.keepAlive"></router-view>
+  <router-view v-else></router-view>
 </template>
 
 <style lang="less">
