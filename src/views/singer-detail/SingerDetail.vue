f2833df6da655830b853845f12d7ec71eb5eda06
xlz122 <122874722@qq.com>
2021-07-23 20:56:46 +0800

    feat: 增加歌手详情路由及文件

---
+++ src/views/singer-detail/SingerDetail.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="singer-detail">
+    <div class="singer-detail-container">
+      <div class="singer-content"></div>
+      <div class="singer-side"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+// import { useRoute } from 'vue-router';
+// import { useStore } from 'vuex';
+// import { playlistDetail } from '@api/song-sheet-detail';
+// import { ResponseType } from '@/types/types';
+
+export default defineComponent({
+  components: {},
+  setup() {
+    // const $route = useRoute();
+    // const $store = useStore();
+    return {};
+  }
+});
+</script>
+
+<style lang="less" scoped>
+@import './singer-detail.less';
+</style>
