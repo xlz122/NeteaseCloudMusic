c3cf78fcf3927ed3125e474798b740558a44e8c7
xlz122 <122874722@qq.com>
2021-07-29 11:13:40 +0800

    feat: 完成歌手详情-相关mv页面

--- src/api/singer-detail.ts
+++ src/api/singer-detail.ts
@@ -19,6 +19,31 @@ export const artistDetail = ({ id }: { id: number }): AxiosRequest => {
 };
 
 /**
+ * @desc 收藏/取消收藏歌手
+ * @param { Number } timestamp - 防止接口缓存
+ * @param { Number } id - 歌手 id
+ * @param { Number } t - 1为收藏,其他为取消收藏
+ */
+
+type ArtistSub = {
+  id: number;
+  t: number;
+};
+
+export const artistSub = ({ id, t }: ArtistSub): AxiosRequest => {
+  const params = {
+    timestamp: new Date().getTime(),
+    id,
+    t
+  };
+  return axios.request({
+    url: '/artist/sub',
+    method: 'get',
+    params
+  });
+};
+
+/**
  * @desc 获取歌手单曲
  * @param { Number } timestamp - 防止接口缓存
  * @param { Number } id - 歌手 id
@@ -62,6 +87,34 @@ export const artistAlbum = ({
   };
   return axios.request({
     url: '/artist/album',
+    method: 'get',
+    params
+  });
+};
+
+/**
+ * @desc 获取歌手mv
+ * @param { Number } timestamp - 防止接口缓存
+ * @param { Number } id - 歌手 id
+ * @param { Number } offset - 页数，默认为 0
+ * @param { Number } limit - 取出数量，默认为 50
+ */
+
+export type ArtistMv = {
+  id: number;
+  offset: number;
+  limit: number;
+};
+
+export const artistMv = ({ id, offset, limit }: ArtistMv): AxiosRequest => {
+  const params = {
+    timestamp: new Date().getTime(),
+    id,
+    offset,
+    limit
+  };
+  return axios.request({
+    url: '/artist/mv',
     method: 'get',
     params
   });
