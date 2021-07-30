4c46d456ee8562d68cae020a8326be1cc4ee2022
xlz122 <122874722@qq.com>
2021-07-30 16:36:37 +0800

    fix: 增加歌曲详情，个人信息文件

---
+++ src/views/song-detail/song-info/SongInfo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="song-user-info-container">
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed } from 'vue';
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
+import { simiPlaylist, simiSong } from '@api/song-detail';
+import { ResponseType } from '@/types/types';
+import SideDownload from '@views/song-sheet-detail/side-downlod/SideDownload.vue';
+
+export default defineComponent({
+  components: {
+    SideDownload
+  },
+  setup() {
+    const $router = useRouter();
+    const $store = useStore();
+
+    return {};
+  }
+});
+</script>
+
+<style lang="less" scoped>
+@import './song-info.less';
+</style>
