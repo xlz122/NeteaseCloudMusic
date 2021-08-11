3c01976179748f59339cd39b2c3e560b9a3640ef
xlz122 <122874722@qq.com>
2021-08-11 15:37:46 +0800

    feat: 首页侧边栏列表数据增加跳转

--- src/views/home/side/Side.vue
+++ src/views/home/side/Side.vue
@@ -9,13 +9,20 @@
         <router-link class="link" to="">查看全部 ></router-link>
       </h3>
       <ul class="list">
-        <li class="item" v-for="(item, index) in singerList" :key="index">
+        <li
+          class="item"
+          v-for="(item, index) in singerList"
+          :key="index"
+          @click="jumpUserProfile(item.userId)"
+        >
           <div class="item-img">
             <img class="img" :src="item.src" alt="" />
           </div>
           <div class="info">
             <h4 class="info-title">{{ item.name }}</h4>
-            <p class="info-desc">{{ item.desc }}</p>
+            <p class="info-desc">
+              {{ item.desc }}
+            </p>
           </div>
         </li>
       </ul>
@@ -27,17 +34,23 @@
         <i class="apply-icon">申请成为网易音乐人</i>
       </a>
       <!-- 热门主播 -->
-      <!-- 热门主播接口未找到 -->
       <h3 class="side-title anchor-title">
         <span>热门主播</span>
       </h3>
       <ul class="list anchor-list">
-        <li class="item" v-for="(item, index) in anchorList" :key="index">
+        <li
+          class="item"
+          v-for="(item, index) in anchorList"
+          :key="index"
+          @click="jumpUserProfile(item.userId)"
+        >
           <div class="item-img">
             <img class="img" :src="item.src" alt="" />
           </div>
           <div class="info">
-            <h4 class="info-title">{{ item.name }}</h4>
+            <h4 class="info-title">
+              {{ item.name }}
+            </h4>
             <p class="info-desc">{{ item.desc }}</p>
           </div>
         </li>
@@ -48,6 +61,8 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
 import SideUser from './side-user/SideUser.vue';
 
 export default defineComponent({
@@ -55,32 +70,40 @@ export default defineComponent({
     SideUser
   },
   setup() {
+    const $router = useRouter();
+    const $store = useStore();
+
     // 入驻歌手无接口(服务器渲染)
     const singerList = ref<unknown[]>([
       {
         src: 'http://p1.music.126.net/rCsLFXha6SLis0tV7yZ5fA==/109951165588539704.jpg?param=62y62',
         name: '张惠妹aMEI',
-        desc: '台湾歌手张惠妹'
+        desc: '台湾歌手张惠妹',
+        userId: 29879272
       },
       {
         src: 'http://p1.music.126.net/wDxCsT3YEYdNg-UZU_ZKeg==/109951166047225823.jpg?param=62y62',
         name: 'Fine乐团',
-        desc: '独立音乐人'
+        desc: '独立音乐人',
+        userId: 100167517
       },
       {
         src: 'http://p1.music.126.net/w_UWOls2uCkFN_U62788Xg==/18806046882229308.jpg?param=62y62',
         name: '萬曉利',
-        desc: '民谣歌手、中国现代民谣的代表人物之一'
+        desc: '民谣歌手、中国现代民谣的代表人物之一',
+        userId: 58426904
       },
       {
         src: 'http://p1.music.126.net/v_zYgE9kmAwVGWV2c8hFxA==/7943971513291094.jpg?param=62y62',
         name: '音乐人赵雷',
-        desc: '民谣歌手'
+        desc: '民谣歌手',
+        userId: 93504818
       },
       {
         src: 'http://p1.music.126.net/NDsX060FnCtiDMZN-c_9Gw==/3438172872737957.jpg?param=62y62',
         name: '王三溥',
-        desc: '音乐人'
+        desc: '音乐人',
+        userId: 46998208
       }
     ]);
     // 热门主播无接口(服务器渲染)
@@ -88,32 +111,45 @@ export default defineComponent({
       {
         src: 'http://p1.music.126.net/H3QxWdf0eUiwmhJvA4vrMQ==/1407374893913311.jpg?param=40y40',
         name: '陈立',
-        desc: '心理学家、美食家陈立教授'
+        desc: '心理学家、美食家陈立教授',
+        userId: 278438485
       },
       {
-        src: 'http://p1.music.126.net/y5-sM7tjnxnu_V9LWKgZlw==/7942872001461517.jpg?param=40y40',
-        name: 'DJ艳秋',
-        desc: '著名音乐节目主持人'
+        src: 'http://p2.music.126.net/GgXkjCzeH4rqPCsrkBV1kg==/109951164843970584.jpg?param=40y40',
+        name: '刘维-Julius',
+        desc: '歌手、播客节目《维维道来》主理人',
+        userId: 559210341
       },
       {
-        src: 'http://p1.music.126.net/6cc6lgOfQTo6ovNnTHPyJg==/3427177769086282.jpg?param=40y40',
-        name: '国家大剧院古典音乐频道',
-        desc: '国家大剧院古典音乐官方'
+        src: 'http://p2.music.126.net/C8jFbOTjUOnTKEHX6uuzOg==/109951166239584597.jpg?param=40y40',
+        name: '莫非定律MoreFeel',
+        desc: '男女双人全创作独立乐团',
+        userId: 259292486
       },
       {
-        src: 'http://p1.music.126.net/xa1Uxrrn4J0pm_PJwkGYvw==/3130309604335651.jpg?param=40y40',
-        name: '谢谢收听',
-        desc: '南京电台主持人王馨'
+        src: 'http://p2.music.126.net/NHjNoFpLDEZ-3OR9h35z1w==/109951165825466770.jpg?param=40y40',
+        name: '碎嘴许美达',
+        desc: '脱口秀网络红人',
+        userId: 1450418799
       },
       {
-        src: 'http://p1.music.126.net/slpd09Tf5Ju82Mv-h8MP4w==/3440371884651965.jpg?param=40y40',
-        name: 'DJ晓苏',
-        desc: '独立DJ，CRI环球旅游广播特邀DJ'
+        src: 'http://p2.music.126.net/CpUdHPNvBvN7kebvL21TTA==/109951163676573083.jpg?param=40y40',
+        name: '银临Rachel',
+        desc: '古风音乐人',
+        userId: 2688170
       }
     ]);
+
+    // 跳转用户资料
+    function jumpUserProfile(userId: number) {
+      // 头部导航取消选中
+      $store.commit('setHeaderActiveIndex', -1);
+      $router.push({ name: 'user-profile', params: { userId } });
+    }
     return {
       singerList,
-      anchorList
+      anchorList,
+      jumpUserProfile
     };
   }
 });
