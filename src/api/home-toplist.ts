37378dce28e35888453b7d6ab5e0bd54ef3ad760
xlz122 <122874722@qq.com>
2021-09-16 16:29:22 +0800

    feat: 完成首页-排行榜页面

---
+++ src/api/home-toplist.ts
@@ -0,0 +1,17 @@
+import axios from '@utils/axios';
+import { AxiosRequest } from '@/types/types';
+
+/**
+ * @desc 获取所有榜单
+ * @param { Number } timestamp - 防止接口缓存
+ */
+export const topList = (): AxiosRequest => {
+  const params = {
+    timestamp: new Date().getTime()
+  };
+  return axios.request({
+    url: '/toplist',
+    method: 'get',
+    params
+  });
+};
