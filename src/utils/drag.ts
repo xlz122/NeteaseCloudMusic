0c3b833cc02c18254e25de2e3b17b3f9dfda2b24
xlz122 <122874722@qq.com>
2021-11-22 14:26:44 +0800

    fix: 拖拽ts类型补齐

--- src/utils/drag.ts
+++ src/utils/drag.ts
@@ -1,11 +1,11 @@
 // 自定义指令 - 弹框拖拽
 export const drag = {
   beforeMount: (
-    el: Record<string, any>,
+    el: HTMLElement,
     binding: Record<string, any>,
-    vnode: Record<string, any>
+    vnode: { el: HTMLElement }
   ): void => {
-    vnode = vnode.el;
+    const vnodeEl = vnode.el;
 
     el.onmousedown = (event: MouseEvent) => {
       const target = event.target as HTMLElement;
@@ -15,24 +15,22 @@ export const drag = {
         // offsetLeft, offsetTop 距离上层或父级的左边距和上边距
 
         // 获取鼠标在弹窗中的位置
-        const mouseX = event.clientX - vnode.offsetLeft;
-        const mouseY = event.clientY - vnode.offsetTop;
+        const mouseX = event.clientX - vnodeEl.offsetLeft;
+        const mouseY = event.clientY - vnodeEl.offsetTop;
 
         // 绑定移动和停止函数
         document.onmousemove = event => {
-          let left, top;
-
           // 获取新的鼠标位置(event.clientX, event.clientY)
           // 弹窗应该在的位置(left, top)
-          left = event.clientX - mouseX;
-          top = event.clientY - mouseY;
+          let left = event.clientX - mouseX;
+          let top = event.clientY - mouseY;
 
           // offsetWidth、offsetHeight 当前元素的宽度
           // innerWidth、innerHeight 浏览器可视区的宽度和高度
 
           // 获取弹窗在页面中距X轴的最小、最大 位置
-          const minX = -vnode.offsetWidth / 2 + 100;
-          const maxX = window.innerWidth + vnode.offsetWidth / 2 - 100;
+          const minX = -vnodeEl.offsetWidth / 2 + 100;
+          const maxX = window.innerWidth + vnodeEl.offsetWidth / 2 - 100;
           if (left <= minX) {
             left = minX;
           } else if (left >= maxX) {
@@ -40,16 +38,17 @@ export const drag = {
           }
 
           // 获取弹窗在页面中距Y轴的最小、最大 位置
-          const minY = vnode.offsetHeight / 2;
-          const maxY = window.innerHeight + vnode.offsetHeight / 2 - 100;
+          const minY = vnodeEl.offsetHeight / 2;
+          const maxY = window.innerHeight + vnodeEl.offsetHeight / 2 - 100;
           if (top <= minY) {
             top = minY;
           } else if (top >= maxY) {
             top = maxY;
           }
+
           // 赋值移动
-          vnode.style.left = left + 'px';
-          vnode.style.top = top + 'px';
+          vnodeEl.style.left = left + 'px';
+          vnodeEl.style.top = top + 'px';
         };
         document.onmouseup = () => {
           document.onmousemove = document.onmouseup = null;
@@ -57,8 +56,8 @@ export const drag = {
       }
     };
     window.onresize = () => {
-      vnode.style.left = '50%';
-      vnode.style.top = '50%';
+      vnodeEl.style.left = '50%';
+      vnodeEl.style.top = '50%';
     };
   }
 };
