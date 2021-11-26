4ef83be1b926277991fc741250e0c1eaa7376e5f
xlz122 <122874722@qq.com>
2021-11-26 16:20:02 +0800

    fix: 修复打包后出现的JSON.pase报错

--- src/store/state.ts
+++ src/store/state.ts
@@ -42,7 +42,7 @@ const state: State = {
   singerId: Number(localStorage.getItem('singerId')) || 0, // 歌手id
   djprogramId: Number(localStorage.getItem('djprogramId')) || 0, // 电台节目id
   albumId: Number(localStorage.getItem('albumId')) || 0, // 专辑id
-  videoId: JSON.parse(localStorage.getItem('videoId') || '') || '', // 视频id
+  videoId: faultTolerant('videoId') || '', // 视频id
   singerTabIndex: Number(localStorage.getItem('singerTabIndex')) || 0, // 歌手详情导航
   searchIndex: Number(localStorage.getItem('searchIndex')) || 0 // 搜索详情导航
 };
