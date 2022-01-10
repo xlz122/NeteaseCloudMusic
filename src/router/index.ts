55d41b48ca2a456b662573043e07484da1e9233a
xlz122 <122874722@qq.com>
2022-01-10 15:01:03 +0800

    feat: 增加等级详情页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -65,6 +65,15 @@ const routes: Array<RouteRecordRaw> = [
             /* webpackChunkName: "user-level" */ '../views/user/level/Level.vue'
           )
       },
+      // 个人中心 - 我的等级 - 等级详情
+      {
+        path: '/level-detail',
+        name: 'level-detail',
+        component: () =>
+          import(
+            /* webpackChunkName: "level-detail" */ '../views/user/level/level-detail/LevelDetail.vue'
+          )
+      },
       // 首页 - 每日推荐
       {
         path: '/home-recommend',
