aa324eafb86e0a12eedc3db2ee728752897a583f
xlz122 <122874722@qq.com>
2022-03-15 15:13:08 +0800

    fix: 修复我的视频跳转mv报错bug

--- src/api/video-detail.ts
+++ src/api/video-detail.ts
@@ -51,3 +51,28 @@ export const videoUrl = ({ id }: { id: number }): AxiosRequest => {
     params
   });
 };
+
+/**
+ * @description 收藏/取消收藏视频
+ * @param { Number } timestamp - 防止接口缓存
+ * @param { Number } id - 视频id
+ * @param { Number } t - t : 1 为收藏,其他为取消收藏
+ */
+
+type VideoSub = {
+  id: number;
+  t: number;
+};
+
+export const videoSub = ({ id, t }: VideoSub): AxiosRequest => {
+  const params = {
+    timestamp: new Date().getTime(),
+    id,
+    t
+  };
+  return axios.request({
+    url: '/video/sub',
+    method: 'get',
+    params
+  });
+};
