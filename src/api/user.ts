986915f39c14d42ed078d22ed8d9c567c31a1c01
xlz122 <122874722@qq.com>
2022-03-30 11:12:59 +0800

    feat: 增加用户主页-听歌排行

--- src/api/user.ts
+++ src/api/user.ts
@@ -17,6 +17,31 @@ export const userAccount = (): AxiosRequest => {
 };
 
 /**
+ * @description 获取用户播放记录
+ * @param { Number } timestamp - 防止接口缓存
+ * @param { Number } uid - 用户id
+ * @param { Number } [type] 1返回 weekData, 0返回 allData
+ */
+
+type UserRecord = {
+  uid: number;
+  type?: number;
+};
+
+export const userRecord = ({ uid, type }: UserRecord): AxiosRequest => {
+  const params = {
+    timestamp: new Date().getTime(),
+    uid,
+    type
+  };
+  return axios.request({
+    url: '/user/record',
+    method: 'get',
+    params
+  });
+};
+
+/**
  * @description 获取用户等级信息
  * @param { Number } timestamp - 防止接口缓存
  */
