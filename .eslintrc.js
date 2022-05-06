343721d2b5fe219e90a458471559a60f1c1cda49
xlz122 <122874722@qq.com>
2022-05-06 09:56:55 +0800

    chore: modify eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,39 +15,39 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    // 关闭类型推断
-    // '@typescript-eslint/no-inferrable-types': 'off',
     // 关闭any类型时的警告
     '@typescript-eslint/no-explicit-any': 'off',
     // 组件name ignores数组为忽略（string[]）
     'vue/multi-word-component-names': ['error', {
       'ignores': []
     }],
-    // 禁止在变量定义之前使用它们 variables(变量)
+
+    // 禁用var，使用let、const
+    'no-var': 2,
+    // 禁止在变量定义之前使用
     '@typescript-eslint/no-use-before-define': [
       2,
       { functions: false, classes: true, variables: true }
     ],
-    // 禁用var，用let和const代替
-    'no-var': 2,
-    // 开启强制单引号
-    'quotes': [2, 'single'],
-    // 强制全等( === 和 !==)
-    'eqeqeq': 2,
-    // 语句强制分号结尾
-    'semi': [2, 'always'],
+    // 禁止出现未使用的变量
     '@typescript-eslint/no-unused-vars': [2],
-    // 箭头函数参数括号，一个参数时可省略括号
-    'arrow-parens': [2, 'as-needed'],
-    // 箭头函数，箭头前后空格
-    'arrow-spacing': [2, { before: true, after: true }],
-    // 禁止对象最后一项逗号
-    'comma-dangle': [2, 'never'],
-    // 单行代码/字符串最大长度
-    'max-len': [2, { code: 120 }],
-    // 缩进2个空格
-    'indent': [2, 2],
-    // 文件末尾强制换行
+    // 强制单引号
+    'quotes': [2, 'single'],
+    // 强制全等(===、!==)
+    'eqeqeq': 2,
+    // 强制语句分号结尾
+    'semi': [2, 'always'],
+    // 强制文件末尾换行
     'eol-last': 2,
+    // 箭头函数参数括号(1个参数不需要,1个以上需要)
+    'arrow-parens': [2, 'as-needed'],
+    // 箭头函数(箭头前后空格)
+    'arrow-spacing': [2, { before: true, after: true }],
+    // 对象末尾是否需要逗号
+    'comma-dangle': [2, 'never'],
+    // 单行代码最大长度
+    'max-len': [2, { code: 120 }],
+    // 空格缩进
+    'indent': [2, 2],
   }
 };
