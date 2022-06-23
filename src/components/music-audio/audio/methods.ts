b9c46e44b6cf776a0344a70ffd3be7ebde6ef32a
xlz122 <122874722@qq.com>
2022-06-23 10:34:05 +0800

    refactor: 重构歌曲上下首切换播放逻辑

--- src/components/music-audio/audio/methods.ts
+++ src/components/music-audio/audio/methods.ts
@@ -1,7 +1,10 @@
+/* eslint-disable */
 import store from '@store/index';
 import { getPlayMusicUrl } from '@api/my-music';
 import { ResponseType, LoopType } from '@/types/types';
 import { PlayMusicItem } from '@store/music/state';
+
+// 歌曲未播放，直接点进度，在点播放，播放进度从零开始而不是从进度开始
 
 /**
  * @description 获取音乐播放链接
@@ -13,6 +16,14 @@ export function getMusicUrl(id: number): Promise<string> {
     getPlayMusicUrl({ id })
       .then((res: ResponseType) => {
         if (res.code === 200) {
+          if (!res?.data[0]?.url) {
+            store.commit('setMessage', {
+              type: 'error',
+              title: '播放链接获取失败'
+            });
+            resolve('');
+          }
+
           resolve(res.data[0].url);
         }
       })
@@ -20,104 +31,146 @@ export function getMusicUrl(id: number): Promise<string> {
   });
 }
 
+let timer: number | null = null;
+
 /**
- * @description 获取上一首歌曲id
+ * @description 上一首歌曲
  */
-export function getPrevMusicId(): Promise<number> {
-  return new Promise(resolve => {
-    (async () => {
-      const playMusicId = store.getters['music/playMusicId'];
-      const playMusicList = store.getters['music/playMusicList'];
+export async function getPrevMusicId(): Promise<boolean | undefined> {
+  const playMusicId = store.getters['music/playMusicId'];
+  const playMusicList = store.getters['music/playMusicList'];
 
-      if (playMusicList.length === 0) {
-        return false;
-      }
-      // 获取当前id索引
-      const index: number = playMusicList.findIndex(
-        (item: LoopType) => item.id === playMusicId
-      );
+  if (playMusicList.length === 0) {
+    return false;
+  }
 
-      let id = 0;
-      // 播放数据不在播放列表（清空播放列表后，重新添加出现这种情况）,播放最后一项
-      if (index === -1) {
-        id = playMusicList.at(-1).id;
-      }
-
-      // 播放数据在播放列表
-      if (index !== -1) {
-        // 不是第一项
-        if (index > 0) {
-          id = playMusicList[index - 1].id;
-        }
-        // 列表回到最后一项
-        if (index === 0) {
-          id = playMusicList.at(-1).id;
-        }
-      }
-
-      // 播放模式
-      const musicModeType = store.getters['music/musicModeType'];
-      // 随机播放
-      if (musicModeType === 2) {
-        id = await randomPlay(playMusicList);
-      }
-
-      // 当前播放音乐
-      const musicItem = playMusicList.find((item: LoopType) => item.id === id);
-      store.commit('music/setPlayMusicItem', musicItem);
-      resolve(id);
-    })();
+  // 重置播放状态
+  store.commit('music/setMusicPlayStatus', {
+    look: false,
+    loading: true
   });
+  // 重置播放进度
+  store.commit('music/setMusicPlayProgress', {
+    progress: 0,
+    currentTime: 0,
+    duration: 0
+  });
+
+  // 获取当前id索引
+  const index: number = playMusicList.findIndex(
+    (item: LoopType) => item.id === playMusicId
+  );
+
+  let id = 0;
+  // 播放数据不在播放列表（清空播放列表后，重新添加出现这种情况）,播放最后一项
+  if (index === -1) {
+    id = playMusicList.at(-1).id;
+  }
+
+  // 播放数据在播放列表
+  if (index !== -1) {
+    // 不是第一项
+    if (index > 0) {
+      id = playMusicList[index - 1].id;
+    }
+    // 列表回到最后一项
+    if (index === 0) {
+      id = playMusicList.at(-1).id;
+    }
+  }
+
+  // 播放模式
+  const musicModeType = store.getters['music/musicModeType'];
+  // 随机播放
+  if (musicModeType === 2) {
+    id = await randomPlay(playMusicList);
+  }
+
+  // 当前播放音乐
+  const musicItem = playMusicList.find((item: LoopType) => item.id === id);
+  store.commit('music/setPlayMusicItem', musicItem);
+
+  // 停止切换后开始播放
+  if (timer) {
+    clearTimeout(timer);
+  }
+  timer = setTimeout(() => {
+    store.commit('music/setMusicPlayStatus', {
+      look: true,
+      loading: false,
+      refresh: true
+    });
+  }, 1000);
 }
 
 /**
- * @description 获取下一首歌曲id
+ * @description 下一首歌曲
  */
-export function getNextMusicId(): Promise<number> {
-  return new Promise(resolve => {
-    (async () => {
-      const playMusicId = store.getters['music/playMusicId'];
-      const playMusicList = store.getters['music/playMusicList'];
+export async function getNextMusicId(): Promise<boolean | undefined> {
+  const playMusicId = store.getters['music/playMusicId'];
+  const playMusicList = store.getters['music/playMusicList'];
 
-      if (playMusicList.length === 0) {
-        return false;
-      }
-      // 获取当前id索引
-      const index: number = playMusicList.findIndex(
-        (item: LoopType) => item.id === playMusicId
-      );
+  if (playMusicList.length === 0) {
+    return false;
+  }
 
-      let id = 0;
-      // 播放数据不在播放列表（清空播放列表后，重新添加出现这种情况）,播放第一项
-      if (index === -1) {
-        id = playMusicList[0].id;
-      }
-
-      // 播放数据在播放列表
-      if (index !== -1) {
-        // 不是最后一项
-        if (index < playMusicList.length - 1) {
-          id = playMusicList[index + 1].id;
-        }
-        // 列表回到第一项
-        if (index !== -1 && index === playMusicList.length - 1) {
-          id = playMusicList[0].id;
-        }
-      }
-
-      // 播放模式
-      const musicModeType = store.getters['music/musicModeType'];
-      // 随机播放
-      if (musicModeType === 2) {
-        id = await randomPlay(playMusicList);
-      }
-
-      // 当前播放音乐
-      const musicItem = playMusicList.find((item: LoopType) => item.id === id);
-      store.commit('music/setPlayMusicItem', musicItem);
-      resolve(id);
-    })();
+  // 重置播放状态
+  store.commit('music/setMusicPlayStatus', {
+    look: false,
+    loading: true
   });
+  // 重置播放进度
+  store.commit('music/setMusicPlayProgress', {
+    progress: 0,
+    currentTime: 0,
+    duration: 0
+  });
+
+  // 获取当前id索引
+  const index: number = playMusicList.findIndex(
+    (item: LoopType) => item.id === playMusicId
+  );
+
+  let id = 0;
+  // 播放数据不在播放列表（清空播放列表后，重新添加出现这种情况）,播放第一项
+  if (index === -1) {
+    id = playMusicList[0].id;
+  }
+
+  // 播放数据在播放列表
+  if (index !== -1) {
+    // 不是最后一项
+    if (index < playMusicList.length - 1) {
+      id = playMusicList[index + 1].id;
+    }
+    // 列表回到第一项
+    if (index !== -1 && index === playMusicList.length - 1) {
+      id = playMusicList[0].id;
+    }
+  }
+
+  // 播放模式
+  const musicModeType = store.getters['music/musicModeType'];
+  // 随机播放
+  if (musicModeType === 2) {
+    id = await randomPlay(playMusicList);
+  }
+
+  // 当前播放音乐
+  const musicItem = playMusicList.find((item: LoopType) => item.id === id);
+  store.commit('music/setPlayMusicItem', musicItem);
+
+  // 停止切换后开始播放
+  if (timer) {
+    clearTimeout(timer);
+  }
+  timer = setTimeout(() => {
+    store.commit('music/setMusicPlayStatus', {
+      look: true,
+      loading: false,
+      refresh: true
+    });
+  }, 1000);
 }
 
 const musicId: number[] = [];
