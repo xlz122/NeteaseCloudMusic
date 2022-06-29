8d8cf8da4b1e6f3609097c4e8daab1d26d6ecb29
xlz122 <122874722@qq.com>
2022-06-29 17:31:51 +0800

    feat: 完成电台节目-更多节目

--- src/views/program-detail/program-side/ProgramSide.vue
+++ src/views/program-detail/program-side/ProgramSide.vue
@@ -5,21 +5,26 @@
       <span class="title-all"> 全部> </span>
     </h3>
     <ul class="program-side-list">
-      <li class="item" v-for="(item, index) in []" :key="index">
+      <li class="item" v-for="(item, index) in list" :key="index">
         <div class="item-cover">
           <img
             class="cover-img"
-            :src="`${item?.coverImgUrl}?param=50y50`"
+            :src="`${item?.coverUrl}?param=50y50`"
             alt=""
+            @click="jumpProgramDetail(item?.id)"
           />
         </div>
         <div class="item-info">
-          <p class="info-title" :title="item?.name">
+          <p
+            class="info-title"
+            :title="item?.name"
+            @click="jumpProgramDetail(item?.id)"
+          >
             {{ item?.name }}
           </p>
           <div class="info-desc">
-            <span class="text" :title="item?.creator?.nickname">
-              {{ item?.creator?.nickname }}
+            <span class="text" :title="item?.serialNum">
+              Vol.{{ item?.serialNum }}
             </span>
           </div>
         </div>
@@ -31,12 +36,61 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
+import { programList } from '@api/program-detail';
+import type { ResponseType } from '@/types/types';
 import SideDownload from '@views/song-sheet-detail/side-downlod/SideDownload.vue';
 
 export default defineComponent({
   components: {
     SideDownload
+  },
+  props: {
+    rid: {
+      typs: Number,
+      default: 0
+    }
+  },
+  setup(props) {
+    const $router = useRouter();
+    const $store = useStore();
+
+    const list = ref([]);
+    // 获取电台节目列表
+    function getProgramList(): void {
+      programList({ rid: props?.rid })
+        .then((res: ResponseType) => {
+          if (res?.code === 200) {
+            list.value = res?.programs?.slice(0, 5);
+          }
+        })
+        .catch(() => ({}));
+    }
+
+    watch(
+      () => props.rid,
+      () => {
+        if (props.rid) {
+          getProgramList();
+        }
+      },
+      {
+        immediate: true
+      }
+    );
+
+    // 跳转电台节目详情
+    function jumpProgramDetail(id: number): void {
+      $router.push({ name: 'program-detail', params: { programId: id } });
+      $store.commit('setProgramId', id);
+    }
+
+    return {
+      list,
+      jumpProgramDetail
+    };
   }
 });
 </script>
