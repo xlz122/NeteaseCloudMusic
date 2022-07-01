f596f0d5b8878fba3bb8ac9897ab9a5fcd146131
Gy-hhh <1025337623@qq.com>
2022-07-01 17:23:14 +0800

    feat: 主播电台-推荐节目、节目排行榜

---
+++ src/views/home-djprogram/radioStation/RadioStation.vue
@@ -0,0 +1,27 @@
+<style lang="less" scoped>
+@import './radioStation.less';
+</style>
+
+<template>
+  <div class="radio-station">
+    <div class="top dis-flex align-center justify-between">
+      <div class="title ff2">
+        <router-link to="">音乐推荐</router-link>
+        <span class="ff1">·</span>
+        电台
+      </div>
+      <router-link to="" class="more">更多 ></router-link>
+    </div>
+    <div class="dis-flex flex-wrap"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {};
+  }
+});
+</script>
