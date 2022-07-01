916f616655eecd33139e6d20ae77988a8139683c
Gy-hhh <1025337623@qq.com>
2022-07-01 17:23:14 +0800

    fix: 首页主播电台-推荐节目、节目排行榜

--- src/api/home-djprogram.ts
+++ src/api/home-djprogram.ts
@@ -15,3 +15,52 @@ export const djCatelist = (): AxiosPromise => {
     params
   });
 };
+
+// 推荐节目
+export const programRecommend = (): AxiosPromise => {
+  const params = {
+    timestamp: new Date().getTime()
+  };
+  return axios.request({
+    url: '/program/recommend',
+    method: 'get',
+    params
+  });
+};
+
+// 节目排行榜
+export type TopListType = {
+  limit?: number;
+  offset?: number;
+};
+export const programTopList = ({
+  limit,
+  offset
+}: TopListType): AxiosPromise => {
+  const params = {
+    timestamp: new Date().getTime(),
+    limit,
+    offset
+  };
+  return axios.request({
+    url: '/dj/program/toplist',
+    method: 'get',
+    params
+  });
+};
+
+// 分类推荐
+export type RecommendType = {
+  type: number;
+};
+export const recommendTypeList = ({ type }: RecommendType): AxiosPromise => {
+  const params = {
+    timestamp: new Date().getTime(),
+    type
+  };
+  return axios.request({
+    url: '/dj/banner',
+    method: 'get',
+    params
+  });
+};
