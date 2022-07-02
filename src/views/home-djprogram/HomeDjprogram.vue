b4856867575a4d85c0a55a40eff78d19e2207d2b
Gy-hhh <1025337623@qq.com>
2022-07-02 15:40:14 +0800

    fix: 主播电台的电台推荐部分

--- src/views/home-djprogram/HomeDjprogram.vue
+++ src/views/home-djprogram/HomeDjprogram.vue
@@ -6,13 +6,17 @@
         <Recommend />
         <RankiList />
       </div>
-      <RadioStation />
+      <RadioStation name="音乐推荐" :musicList="musicObj.musicList" />
+      <RadioStation name="生活" :musicList="musicObj.lifeList" />
+      <RadioStation name="情感" :musicList="musicObj.emotionList" />
+      <RadioStation name="创作翻唱" :musicList="musicObj.coverList" />
+      <RadioStation name="知识" :musicList="musicObj.knowledgeList" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive } from 'vue';
 import { setMessage } from '@/components/message/useMessage';
 import Category from './category/Category.vue';
 import Recommend from './recommend/ReCommend.vue';
@@ -20,6 +24,20 @@ import RankiList from './rankiList/RankiList.vue';
 import RadioStation from './radioStation/RadioStation.vue';
 import { djCatelist, recommendTypeList } from '@api/home-djprogram';
 import type { ResponseType } from '@/types/types';
+
+type MusicType = {
+  djList: object[];
+  musicList: object[];
+  musicId: number;
+  lifeList: object[];
+  lifeId: number;
+  emotionList: object[];
+  emotionId: number;
+  coverList: object[];
+  coverId: number;
+  knowledgeList: object[];
+  knowledgeId: number;
+};
 
 export default defineComponent({
   name: 'home-djprogram',
@@ -34,22 +52,82 @@ export default defineComponent({
     function djCategorChange(id: number): void {
       setMessage({ type: 'info', title: `分类id:${id}` });
     }
+    // 获取全部电台类型
+    function djcateHandle(): Promise<object[]> {
+      return new Promise((resolve, reject) => {
+        djCatelist()
+          .then((res: ResponseType) => {
+            if (res?.code === 200) {
+              resolve(res.categories);
+            } else {
+              reject();
+            }
+          })
+          .catch(() => reject());
+      });
+    }
+    // 根据电台类型id获取对应推荐
+    function recommendTypeHandle(id: number): Promise<object[]> {
+      return new Promise((resolve, reject) => {
+        recommendTypeList({ type: id })
+          .then((res: ResponseType) => {
+            if (res?.code === 200) {
+              resolve(res.djRadios);
+            } else {
+              reject();
+            }
+          })
+          .catch(() => reject());
+      });
+    }
 
-    function djcateHandle() {
-      djCatelist().then((res: ResponseType) => {
-        console.log('res1', res);
-      });
+    const musicObj = reactive<MusicType>({
+      djList: [],
+      musicList: [],
+      lifeId: 0,
+      lifeList: [],
+      musicId: 0,
+      emotionList: [],
+      emotionId: 0,
+      coverList: [],
+      coverId: 0,
+      knowledgeList: [],
+      knowledgeId: 0
+    });
+
+    async function recommendTypeHandleFn() {
+      musicObj.djList = await djcateHandle();
+      musicObj.musicId = (
+        musicObj.djList.filter((x: any) => x.name === '音乐推荐')[0] as any
+      ).id;
+      musicObj.musicList = await recommendTypeHandle(musicObj.musicId);
+
+      musicObj.lifeId = (
+        musicObj.djList.filter((x: any) => x.name === '生活')[0] as any
+      ).id;
+      musicObj.lifeList = await recommendTypeHandle(musicObj.lifeId);
+
+      musicObj.emotionId = (
+        musicObj.djList.filter((x: any) => x.name === '情感')[0] as any
+      ).id;
+      musicObj.emotionList = await recommendTypeHandle(musicObj.emotionId);
+
+      musicObj.coverId = (
+        musicObj.djList.filter((x: any) => x.name === '创作翻唱')[0] as any
+      ).id;
+      musicObj.coverList = await recommendTypeHandle(musicObj.coverId);
+
+      musicObj.knowledgeId = (
+        musicObj.djList.filter((x: any) => x.name === '知识')[0] as any
+      ).id;
+      musicObj.knowledgeList = await recommendTypeHandle(musicObj.knowledgeId);
+      console.log(musicObj.knowledgeList);
     }
-    djcateHandle();
-    function recommendTypeHandle() {
-      recommendTypeList({ type: 2 }).then((res: ResponseType) => {
-        console.log('res', res);
-      });
-    }
-    recommendTypeHandle();
+    recommendTypeHandleFn();
 
     return {
-      djCategorChange
+      djCategorChange,
+      musicObj
     };
   }
 });
--- src/views/home-djprogram/radioStation/RadioStation.vue
+++ src/views/home-djprogram/radioStation/RadioStation.vue
@@ -6,13 +6,27 @@
   <div class="radio-station">
     <div class="top dis-flex align-center justify-between">
       <div class="title ff2">
-        <router-link to="">音乐推荐</router-link>
+        <router-link to="">{{ name }}</router-link>
         <span class="ff1">·</span>
         电台
       </div>
       <router-link to="" class="more">更多 ></router-link>
     </div>
-    <div class="dis-flex flex-wrap"></div>
+    <div class="dis-flex flex-wrap">
+      <div
+        class="rdilist dis-flex"
+        v-for="item in musicList.slice(0, 4)"
+        :key="item.id"
+      >
+        <div class="picUrl pointer">
+          <img :src="item.picUrl" alt="电台图片" />
+        </div>
+        <div class="cnt">
+          <div class="name" :title="item.name">{{ item.name }}</div>
+          <div class="note">{{ item.rcmdtext }}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,6 +34,16 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    musicList: {
+      type: Array,
+      default: () => []
+    }
+  },
   setup() {
     return {};
   }
