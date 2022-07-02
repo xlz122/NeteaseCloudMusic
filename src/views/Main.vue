808287b4ba25f913fb12b1fa37a9bee5a600b1c8
xlz122 <122874722@qq.com>
2022-07-02 16:39:30 +0800

    fix(css): 解决pc端滚动条跳动样式,在移动端需还原样式

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted } from 'vue';
 import HeaderView from '@views/header/Header.vue';
 import FooterView from '@views/footer/Footer.vue';
 import JumpTop from '@components/jump-top/JumpTop.vue';
@@ -22,6 +22,28 @@ export default defineComponent({
     HeaderView,
     FooterView,
     JumpTop
+  },
+  setup() {
+    const userAgent = navigator.userAgent.toLowerCase();
+
+    onMounted(() => {
+      const reg =
+        /ipad|iphone|midp|rv:1.2.3.4|ucweb|android|windows ce|windows mobile/;
+      const header = document.querySelector('.header') as HTMLElement;
+      const munuBg = document.querySelector('.h-top-bg') as HTMLElement;
+      const subMenuBg = document.querySelector('.sub-menu-bg') as HTMLElement;
+      const audioBg = document.querySelector(
+        '.music-audio-playbar'
+      ) as HTMLElement;
+
+      // 解决pc端滚动条跳动样式,在移动端需还原样式
+      if (reg.test(userAgent)) {
+        header.style.width = '100%';
+        munuBg.style.width = '100%';
+        subMenuBg.style.width = '100%';
+        audioBg.style.width = '100%';
+      }
+    });
   }
 });
 </script>
