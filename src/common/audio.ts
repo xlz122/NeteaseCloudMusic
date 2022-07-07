3d090c5b7c9e74ceaacd9a5c1498de4805e7754a
xlz122 <122874722@qq.com>
2022-07-07 11:16:24 +0800

    chore: 完善播放单个音乐、添加到播放器函数的ts类型

--- src/common/audio.ts
+++ src/common/audio.ts
@@ -1,5 +1,4 @@
 import type { PlayMusicItem } from '@store/music/state';
-import { toRawType } from '@utils/tool';
 
 export type SongType = {
   id: number;
@@ -28,11 +27,9 @@ export type SongType = {
  * @description 歌曲数据处理成音乐播放器所需数据
  * @param { Object } song 歌曲数据
  */
-export function handleAudioSong(song: Partial<SongType>): PlayMusicItem {
-  if (toRawType(song) !== 'Object') {
-    throw new Error('添加的歌曲数据类型错误');
-  }
-
+export function handleAudioSong<T extends Partial<SongType>>(
+  song: T
+): PlayMusicItem {
   const musicItem: PlayMusicItem = {
     id: song?.id || 0,
     name: song?.name || '',
@@ -50,16 +47,6 @@ export function handleAudioSong(song: Partial<SongType>): PlayMusicItem {
       name: item.name
     });
   });
-
-  // 播放器数据直接返回
-  const keys1 = Object.keys(musicItem);
-  const keys2 = Object.keys(song);
-
-  const isCongruence = keys1.every(val => keys2.includes(val));
-
-  if (isCongruence) {
-    return song as PlayMusicItem;
-  }
 
   return musicItem;
 }
