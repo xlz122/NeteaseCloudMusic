e17b1bcb80f73df6a8c7e8c6671312b95caae62b
xlz122 <122874722@qq.com>
2025-04-03 16:53:32 +0800

    refactor: vue cli -> vite

---
+++ src/vite-env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types='vite/client' />
+
+declare module 'vuex' {
+  export * from 'vuex/types/index.d.ts';
+  export * from 'vuex/types/helpers.d.ts';
+  export * from 'vuex/types/logger.d.ts';
+  export * from 'vuex/types/vue.d.ts';
+}
+declare module 'lodash';
